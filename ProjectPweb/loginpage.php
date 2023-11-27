ebe0e98c36998438691ea2896069859d247def3e
Tantrawww <116472946+Tantrawww@users.noreply.github.com>
2023-11-27 22:26:55 +0700

    Add files via upload

---
+++ ProjectPweb/loginpage.php
@@ -0,0 +1,20 @@
+<?php
+$hostname = "localhost";
+$user = "root";
+$password = "";
+$db_name = "projectweb";
+
+$koneksi = mysqli_connect($hostname, $user, $password, $db_name) or die("". mysqli_error($koneksi));
+
+$username = $_POST['username'];
+$password1 = $_POST['password1'];
+
+$query_sql = "SELECT * from user where username = '$username' AND password_user = '$password1'";
+
+$result = mysqli_query($koneksi, $query_sql);
+
+if (mysqli_num_rows($result) > 0) {
+    header("Location: coba2.html");
+} else {
+    echo"<center><h1>Email atau Password salah.<h1>";
+}
